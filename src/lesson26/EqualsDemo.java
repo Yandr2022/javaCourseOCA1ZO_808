76a32fdc807773eb623ca3c91c13153f069eb58f
Yandr2022 <sharkievich@gmail.com>
2023-04-30 11:53:07 +0300

    lesson 26

--- src/lesson26/EqualsDemo.java
+++ src/lesson26/EqualsDemo.java
@@ -1,26 +1,24 @@
 package lesson26;
 
-import java.util.Objects;
-
 public class EqualsDemo {
     public static void main(String[] args) {
         Car c1 = new Car("red", "V2");
         Car c2 = new Car("red", "V2");
-        Car c3 = new Car("black", "V2");
+        Car c3 = new Car("black", "V3");
         Car c4 = c2;
-
         System.out.println(c1 == c2);
-        System.out.println(c1.equals(c2) + "\n");
-
-        System.out.println(c3 == c2);
-        System.out.println(c3.equals(c2) + "\n");
-
-        System.out.println(c4 == c2);
-        System.out.println(c4.equals(c2) + "\n");
-
-        System.out.println(c1.equals(null));
-        c1 = null;
         System.out.println(c1.equals(c2));
+
+        System.out.println(c1==c3);
+        System.out.println(c1.equals(c3));
+
+        System.out.println(c2 == c4);
+        System.out.println(c4.equals(c2));
+
+        Car[]cars = {c1, c2, c3,c4};
+        for (Car car:cars) {
+            System.out.println(car);
+        }
 
     }
 }
@@ -35,13 +33,13 @@ class Car {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (o instanceof Car) {
-            Car car = (Car) o;
-//            return Objects.equals(color, car.color) && Objects.equals(engine, car.engine);
-            return color == car.color && engine == car.engine;
+    public boolean equals(Object obj) {
+        if (obj instanceof Car) {
+            Car car = (Car) obj;
+            return color.equals(car.color) && engine.equals(car.engine);
+        } else {
+            return false;
         }
-        return false;
     }
 
     @Override
